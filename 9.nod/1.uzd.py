851acd339a2085265d5abdaf048bc2cb5b0cc403
Kaspars Rumbens <krumba@inbox.lv>
2024-05-30 20:51:48 +0300

    trtuy

--- 9.nod/1.uzd.py
+++ 9.nod/1.uzd.py
@@ -1,7 +1,7 @@
 class Song:
   """Klases definīcija dziesmām."""
 
-  def __init__(self, title, author, lyrics, max_lines=-1,):
+  def __init__(self, title, author, lyrics, max_lines=-1):
     """Konstruktors dziesmas izveidei."""
     self.title = title
     self.author = author
@@ -21,22 +21,20 @@ class Song:
   def yell(self, max_lines=None):
     """Izdrukā dziesmu ar lieliem burtiem pa rindiņai."""
     if self.title and self.author:
-        print(f"{self.title} - {self.author}")
+      print(f"{self.title} - {self.author}")
     if max_lines is None:
-        max_lines = self.max_lines
+      max_lines = self.max_lines
     for line in self.lyrics[:max_lines]:
-        words = line.split()
-        upper_case_line = ' '.join([word.upper() for word in words])
-        print(upper_case_line)
+      words = line.split()
+      upper_case_line = ' '.join([word.upper() for word in words])
+      print(upper_case_line)
 
-# Piemērs dziesmas izveidei un izdrukai
+# Piemērs dziesmu izveidei un izdrukai
 ziemelmeita = Song("Ziemeļmeita", "Jumprava", ["Gāju meklēt ziemeļmeitu", "Garu, tālu ceļu veicu"])
 ziemelmeita.sing(1)  # Izdrukā pirmo rindiņu
 ziemelmeita.sing()    # Izdrukā visas rindas
 ziemelmeita.yell()   # Izdrukā visu dziesmu ar lieliem burtiem
 
-# Piemērs ar citu dziesmu
 meza_meita = Song("Meža meita", "Taurenīte", ["Dzied meža meitiņa", "Pļavās zied zālīte"])
 meza_meita.sing(2)   # Izdrukā pirmās divas rindas
 meza_meita.yell(1)  # Izdrukā pirmo rindiņu ar lieliem burtiem
-
